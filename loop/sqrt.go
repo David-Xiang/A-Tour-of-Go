e7efb4caa5c3c295ab35448fda7cd32b6a684770
David Xiang <xiangdongwei@pku.edu.cn>
2019-10-02 16:20:31 +0800

    add loop&function exercise -- sqrt.go

---
+++ loop/sqrt.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "fmt"
+)
+
+func Sqrt(x float64) (z float64) {
+    z = 1
+    for Abs(z * z - x) > 1e-6 {
+        z -= (z * z - x) / (2 * z)
+    }
+    return
+}
+
+func Abs(x float64) float64 {
+    if x > 0 {
+        return x
+    }
+    return -x
+}
+
+func main() {
+    fmt.Println(Sqrt(2))
+}
